b962f3277a68bba08ef2273c5ed0f7f48d4b4962
albertcast <48439879+albertcast@users.noreply.github.com>
2022-11-14 19:08:12 +0100

    home

---
+++ src/main/resources/static/js/listaUsuarios.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $.ajax({
+      	url: "https://pisoswebserver.herokuapp.com/api/user/",
+		success: function(data){			
+			 let output = data.map(i => "<tr><th scope='row'>" + i.email + "</td><td>" + i.nombre 
+										+ "</td><td>" + i.apellidos + "</td><td>" + i.edad + "</td></tr>");
+			document.getElementById("tablaUser").innerHTML += output;
+		}
+    })
+});	
+
