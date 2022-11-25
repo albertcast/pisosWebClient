b56e4243efbd315ebba8d09a41e0675fe7eba51a
albertcast <48439879+albertcast@users.noreply.github.com>
2022-11-25 18:25:28 +0100

    misPropiedades iniciado
    Quitado también una fuente de script que devolvia 404

---
+++ src/main/resources/static/js/misPropiedades.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $.ajax({
+      	url: "https://pisoswebserver.herokuapp.com/api/apartment/owner?owner=63641e030a34a82ae3b7f137",
+		success: function(data){		
+			let output = data.map(i => "<tr class='clickable' onclick='sendToDetalles("+ '"'+ i.id + '"' + ")'><th scope='row'>" + i.title + "</th><td>" + i.place
+										+ "</td><td>" + i.description+ "</td><td>" + i.date + "</td></tr>");
+			document.getElementById("tablaAparment").innerHTML += output;
+		}
+    })
+});	
