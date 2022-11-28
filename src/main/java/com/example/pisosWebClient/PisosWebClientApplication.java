b4ec317082153835bc115d69b47c3f3bc5113903
albertcast <prophet112233@gmail.com>
2022-11-28 13:00:50 +0100

    login

--- src/main/java/com/example/pisosWebClient/PisosWebClientApplication.java
+++ src/main/java/com/example/pisosWebClient/PisosWebClientApplication.java
@@ -1,19 +1,30 @@
 package com.example.pisosWebClient;
 
+import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,13 +32,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PisosWebClientApplication  {
 
+	
+	
 	@GetMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-		if(principal!= null)
-			return Collections.singletonMap("name", principal.getAttribute("name"));
+		if(principal!= null) {
+			Map<String,Object> mapa;
+			mapa = new HashMap<String, Object>();
+			mapa.put("accountAuthentication", principal.getAttribute("id"));
+			mapa.put("email", principal.getAttribute("email"));
+			mapa.put("name", principal.getAttribute("name"));
+			mapa.put("lastname", "");
+			mapa.put("age", 0);
+			mapa.put("string", principal.getAttributes().toString());
+
+
+			return mapa;
+		}
 		return null;
     }
-	
 	
 	
 	@Bean
