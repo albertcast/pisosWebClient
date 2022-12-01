a772627c3c98b230ed643bcab05a3971d376d785
alejandrogc259 <alejandrogc259@gmail.com>
2022-12-01 19:23:57 +0100

    Comentarios funcionales

--- src/main/resources/static/js/listaComentarios.js
+++ src/main/resources/static/js/listaComentarios.js
@@ -1,47 +1,77 @@
 
-function usuario(id) {
-	nombre=""
-    $.ajax({
-        url: "https://pisoswebserver.herokuapp.com/api/user/"+id
-    }).then(function(data) {
-       $('.user-name').append(data.usuarioId);
-       nombre=data.nombre
-       console.log(data)
-       alert(nombre)
-       $('.user-lastname').append(data.email);
-    });
-    return nombre
-};
+function estrellas(rating){
+	let i
+	let output=""
+	alert(rating)
+	for(i=0;i<rating;i++){
+		output+="<i class='icon_star'></i>";
+	}
+	if(i!=5){
+		while(i<5){
+			output+="<i class='icon_star' style='color: black'></i>";
+			i++;
+		}
+	}
+	return output
+}
+
+function botonBorrar(id,idComentario){
+	if(id==sessionStorage.userId){
+		let output="<button onclick='javascript:borrar(\""+idComentario+"\")' style='border: 2px solid black;color: black;background-color: orange;' ><b>Borrar comentario</b></button>"
+		return output;
+	}
+	else return ""
+}
+
+function borrar(id){
+	$.ajax({
+			type:"DELETE",
+			url: "https://pisoswebserver.herokuapp.com/api/comment/delete/"+id,
+			success: function(msg){
+				alert("SE HA ELIMINADO EL COMENTARIO CORRECTAMENTE");
+			}
+		}).then(function(datos){
+			location.href = "room-details.html";
+		})
+}
+
 
 
 
 
 $(document).ready(function() {
-
+ 
     $.ajax({
-      	url: "https://pisoswebserver.herokuapp.com/api/comment/apartment/"+window.sessionStorage.apartmentId,
-		success: function(data){		
-										
-			let output = data.map(i => "<div class='review-item'>"+
+        type:"GET",
+          url: "https://pisoswebserver.herokuapp.com/api/comment/apartment/"+window.sessionStorage.apartmentId,
+        success: function(data){
+
+            data.map(i => (
+                $.ajax({
+                    type:"GET",
+                       url: "https://pisoswebserver.herokuapp.com/api/user/"+i.user,
+                }).then(function(datos){
+                    let output = "<div class='review-item'>"+
                             "<div class='ri-pic'>"+
                                 "<img src='img/room/avatar/avatar-1.jpg' alt=''>"+
                             "</div>"+
                             "<div class='ri-text'>"+
-                                "<span>METER FECHA EN COMENTARIOS</span>"+
+                                "<span>"+i.date+"</span>"+
                                 "<div class='rating'>"+
-                                    "<i class='icon_star'></i>"+
-                                    "<i class='icon_star'></i>"+
-                                    "<i class='icon_star'></i>"+
-                                    "<i class='icon_star'></i>"+
-                                    "<i class='icon_star-half_alt'></i>"+
+                                    estrellas(i.rating)+
                                 "</div>"+
-                                "<h5>"+String(usuario(i.user))+"</h5>"+
+                                "<h5>"+datos.nombre.toUpperCase()+" "+datos.apellidos.toUpperCase()+"</h5>"+
                                 "<p>"+i.text+"</p>"+
                             "</div>"+
-                        "</div>").join('');
-     
-                
-			document.getElementById("tablaComment").innerHTML += output;
-		}
+                            botonBorrar(i.user,i.id)+
+                        "</div>"
+
+                        document.getElementById("tablaComment").innerHTML += output;
+
+                        })
+                        )).join('');
+
+
+        }
     })
 });
