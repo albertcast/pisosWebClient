9debfe5b3b973d5d1e7a9c66794456c28330a7b3
albertcast <prophet112233@gmail.com>
2023-01-08 21:27:29 +0100

    a

--- src/main/java/com/example/pisosWebClient/PisosWebClientApplication.java
+++ src/main/java/com/example/pisosWebClient/PisosWebClientApplication.java
@@ -47,7 +47,7 @@ public class PisosWebClientApplication  {
 			mapa.put("age", 0);
 			mapa.put("avatar_url", principal.getAttribute("avatar_url"));
 			mapa.put("string", principal.getAttributes().toString());
-			mapa.put("token", session);
+			
 
 			return mapa;
 		}
